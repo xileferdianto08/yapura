07240ea65124f8dceb4a2e06afdcda92f0d95a0f
Bryant CH <bchihola@gmail.com>
2022-12-07 00:09:09 +0700

    Admin Menu
    Upload
    Remove

---
+++ app/src/main/java/id/ac/umn/yapura/AdminPage.java
@@ -0,0 +1,17 @@
+package id.ac.umn.yapura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+
+
+
+    }
+}
