21c0bd19533bf5f89744fb65344623cb758b5c43
Jovan Haliem <jovan.haliem@student.umn.ac.id>
2022-12-06 20:42:30 +0700

    updatee lurr

---
+++ app/src/main/java/id/ac/umn/yapura/DetailRuangan.java
@@ -0,0 +1,14 @@
+package id.ac.umn.yapura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailRuangan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_ruangan);
+    }
+}
