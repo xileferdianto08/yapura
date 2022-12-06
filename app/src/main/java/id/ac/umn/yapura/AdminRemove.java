424d89535282189816c2cc05b7673d804de97a8f
Bryant CH <bchihola@gmail.com>
2022-12-07 01:21:48 +0700

    Upload Gambar
    Admin upload gambar

---
+++ app/src/main/java/id/ac/umn/yapura/AdminRemove.java
@@ -0,0 +1,14 @@
+package id.ac.umn.yapura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminRemove extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_remove);
+    }
+}
---
+++ app/src/main/java/id/ac/umn/yapura/AdminUploadImage.java
@@ -0,0 +1,15 @@
+package id.ac.umn.yapura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminUploadImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_image);
+        
+    }
+}
