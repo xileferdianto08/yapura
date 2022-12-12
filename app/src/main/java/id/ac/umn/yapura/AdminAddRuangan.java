1ae57f03a3abe5b516811fe23fc3fa995425c0b8
Jovan Haliem <jovan.haliem@student.umn.ac.id>
2022-12-12 22:45:05 +0700

    updateee lagii

---
+++ app/src/main/java/id/ac/umn/yapura/AdminAddRuangan.java
@@ -0,0 +1,14 @@
+package id.ac.umn.yapura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminAddRuangan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_ruangan);
+    }
+}
