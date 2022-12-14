4bd2f638dc34f2848a2a093cc6fd72722d5c64a9
Bryant CH <bchihola@gmail.com>
2022-12-15 00:57:04 +0700

    Upload & Capture Image
    upload & capture image using camera

--- app/src/main/java/id/ac/umn/yapura/AdminUploadImage.java
+++ app/src/main/java/id/ac/umn/yapura/AdminUploadImage.java
@@ -12,21 +12,23 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 
 import java.io.IOException;
 
 public class AdminUploadImage extends AppCompatActivity {
 
-    private Button uploadImage;
+    private Button uploadImage, captureImage;
     private ImageView uploadImageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private final int REQ = 1;
+    private final int REQ = 2;
     private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
-
+        captureImage = findViewById(R.id.captureImage);
         uploadImage = findViewById(R.id.uploadImageBtn);
         uploadImageView = findViewById(R.id.uploadImageView);
 
@@ -36,7 +38,17 @@ public class AdminUploadImage extends AppCompatActivity {
                 openGallery();
             }
         });
-        
+
+
+        captureImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(getPackageManager())!= null){
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 
     private void openGallery() {
@@ -47,11 +59,15 @@ public class AdminUploadImage extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQ && resultCode == RESULT_OK){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            uploadImageView.setImageBitmap(imageBitmap);
+        }else if(requestCode == REQ && resultCode == RESULT_OK){
             Uri uri = data.getData();
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
-            } catch (IOException e) {
+            }catch (IOException e){
                 e.printStackTrace();
             }
             uploadImageView.setImageBitmap(bitmap);
